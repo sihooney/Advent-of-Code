f94493edc4b8eb25061782f667763e3261511175
sihooney <84606621+sihooney@users.noreply.github.com>
2024-09-16 21:27:56 -0400

    Update star1.py

--- aoc2023/day09/star1.py
+++ aoc2023/day09/star1.py
@@ -1,3 +1,10 @@
-l = [[int(i) for i in s.split()] for s in open('day9.txt')]
-def r(m): return m[-1] + r([b-a for a, b in zip(m, m[1:])]) if any(m) else 0
-print(sum(r(i) for i in l))
+data = [[int(x) for x in s.split()] for s in open('day9.txt')]
+ans = 0
+for a in data:
+    ans += a[-1]
+    while any(a) is True:
+        for i in range(len(a) - 1):
+            a[i] = a[i + 1] - a[i]
+        a.pop()
+        ans += a[-1]
+print(ans)
