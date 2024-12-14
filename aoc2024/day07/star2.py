9f8b525c334e1f4c615958b06d9021f5e98c0e50
sihooney <84606621+sihooney@users.noreply.github.com>
2024-12-14 14:18:42 -0500

    Update star2.py

--- aoc2024/day07/star2.py
+++ aoc2024/day07/star2.py
@@ -1,18 +1,18 @@
 from itertools import product
 
 
-all_combs = [list(product([0, 1, 2], repeat=n)) for n in range(12)]
+permutations = [list(product([0, 1, 2], repeat=n)) for n in range(12)]
 data = open('day7.txt').readlines()
 ans = 0
 for line in data:
     val = int(line[:line.index(':')])
     a = list(map(int, line[line.index(':') + 1:].split()))
     res = a[0]
-    for comb in all_combs[len(a) - 1]:
-        for i in range(len(comb)):
-            if comb[i] == 0:
+    for p in permutations[len(a) - 1]:
+        for i in range(len(p)):
+            if p[i] == 0:
                 res += a[i + 1]
-            elif comb[i] == 1:
+            elif p[i] == 1:
                 res *= a[i + 1]
             else:
                 res = int(str(res) + str(a[i + 1]))
