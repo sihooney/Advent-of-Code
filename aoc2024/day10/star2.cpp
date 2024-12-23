b49506f0e60e2a0bacc8fe1ec480c10ac4f35b57
sihooney <84606621+sihooney@users.noreply.github.com>
2024-12-23 18:49:42 -0500

    Create star2.cpp

---
+++ aoc2024/day10/star2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+const int N = 60;
+int a[N][N];
+pair<int, int> dir[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int ans = 0;
+
+void dfs(int r, int c, int prev)
+{
+    if (r >= 0 && r < N && c >= 0 && c < N && a[r][c] == prev + 1)
+    {
+        if (a[r][c] == 9)
+        {
+            ans++;
+            return;
+        }
+        for (const auto &[dr, dc] : dir)
+        {
+            dfs(r + dr, c + dc, prev + 1);
+        }
+    }
+}
+
+int main()
+{
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<pair<int, int>> src;
+    ifstream input("day10.txt");
+    char c;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            input >> c;
+            a[i][j] = c - '0';
+            if (a[i][j] == 0)
+            {
+                src.push_back({i, j});
+            }
+        }
+    }
+    for (const auto &[r, c] : src)
+    {
+        dfs(r, c, -1);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
