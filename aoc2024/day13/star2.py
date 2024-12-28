fa36ce08d687802003d82c012d29a4aa6cb023d8
sihooney <84606621+sihooney@users.noreply.github.com>
2024-12-28 15:48:17 -0500

    Update star2.py

--- aoc2024/day13/star2.py
+++ aoc2024/day13/star2.py
@@ -2,8 +2,7 @@ from numpy import linalg
 from re import findall
 
 
-data = list(map(lambda s: list(map(int, findall(r'\d+', s))),
-            open("day13.txt").read().split("\n\n")))
+data = list(map(lambda s: list(map(int, findall(r'\d+', s))), open("day13.txt").read().split("\n\n")))
 ans = 0
 for arr in data:
     sol = linalg.solve([[arr[0], arr[2]], [arr[1], arr[3]]], [
