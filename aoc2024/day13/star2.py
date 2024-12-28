7d5e004dd49bd83fb5210b4f26fb832a6d08eb25
sihooney <84606621+sihooney@users.noreply.github.com>
2024-12-28 15:48:34 -0500

    Update star2.py

--- aoc2024/day13/star2.py
+++ aoc2024/day13/star2.py
@@ -5,8 +5,7 @@ from re import findall
 data = list(map(lambda s: list(map(int, findall(r'\d+', s))), open("day13.txt").read().split("\n\n")))
 ans = 0
 for arr in data:
-    sol = linalg.solve([[arr[0], arr[2]], [arr[1], arr[3]]], [
-                       arr[4] + 1e13, arr[5] + 1e13])
+    sol = linalg.solve([[arr[0], arr[2]], [arr[1], arr[3]]], [arr[4] + 1e13, arr[5] + 1e13])
     if abs(sol[0] - round(sol[0])) < 1e-4 and abs(sol[1] - round(sol[1])) < 1e-4:
         ans += 3 * sol[0] + sol[1]
 print(int(ans))
