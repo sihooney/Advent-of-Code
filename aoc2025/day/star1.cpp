a665cc248b52b0055881255beed7a1abb41fca68
Sihan Qian <84606621+sihooney@users.noreply.github.com>
2025-12-03 16:17:19 -0500

    Create star1.cpp

---
+++ aoc2025/day/star1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ifstream in("day2.txt");
+    char c;
+    ll a, b, ans{0};
+    while (in >> a >> c >> b)
+    {
+        for (ll x = a; x <= b; ++x)
+        {
+            string s{to_string(x)};
+            ans += (s.substr(0, s.length() / 2) == s.substr(s.length() / 2)) * x;
+        }
+        in >> c;
+    }
+    cout << ans << endl;
+    return 0;
+}
