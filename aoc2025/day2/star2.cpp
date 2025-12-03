2f9ba933f0eefffa2261ff152c8a6bc6eab8b87b
Sihan Qian <84606621+sihooney@users.noreply.github.com>
2025-12-03 16:19:29 -0500

    Add star2.cpp for processing input and validation

---
+++ aoc2025/day2/star2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using ll = long long;
+
+bool is_invalid(string s)
+{
+    for (int len = s.length() / 2; len >= 1; --len)
+    {
+        if (s.length() % len != 0)
+        {
+            continue;
+        }
+        bool repeat{true};
+        for (int i = 0; i < len; ++i)
+        {
+            if (!repeat)
+            {
+                break;
+            }
+            for (int j = i + len; j < s.length(); j += len)
+            {
+                if (s[j] != s[i])
+                {
+                    repeat = false;
+                    break;
+                }
+            }
+        }
+        if (repeat)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    ifstream in("day2.txt");
+    char c;
+    ll a, b, ans{0};
+    while (in >> a >> c >> b)
+    {
+        for (ll x = a; x <= b; ++x)
+        {
+            ans += is_invalid(to_string(x)) * x;
+        }
+        in >> c;
+    }
+    cout << ans << endl;
+    return 0;
+}
