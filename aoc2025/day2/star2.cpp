0e23b0599cfb163a000049a9ea0fa8ce3744514d
Sihan Qian <84606621+sihooney@users.noreply.github.com>
2025-12-05 23:56:25 -0500

    Refactor loop variable initialization syntax

--- aoc2025/day2/star2.cpp
+++ aoc2025/day2/star2.cpp
@@ -6,20 +6,20 @@ using ll = long long;
 
 bool is_invalid(string s)
 {
-    for (size_t len = s.length() / 2; len >= 1; --len)
+    for (size_t len{s.length() / 2}; len >= 1; --len)
     {
         if (s.length() % len != 0)
         {
             continue;
         }
         bool repeat{true};
-        for (size_t i = 0; i < len; ++i)
+        for (size_t i{0}; i < len; ++i)
         {
             if (!repeat)
             {
                 break;
             }
-            for (size_t j = i + len; j < s.length(); j += len)
+            for (size_t j{i + len}; j < s.length(); j += len)
             {
                 if (s[j] != s[i])
                 {
@@ -43,7 +43,7 @@ int main()
     ll a, b, ans{0};
     while (in >> a >> c >> b)
     {
-        for (ll x = a; x <= b; ++x)
+        for (ll x{a}; x <= b; ++x)
         {
             ans += is_invalid(to_string(x)) * x;
         }
