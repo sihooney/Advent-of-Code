17f10b101719f7fb53175e8f7ebac8dec7e36639
Sihan Qian <84606621+sihooney@users.noreply.github.com>
2025-12-06 17:28:18 -0500

    Add solution for Day 5, Star 1 of AoC 2025

---
+++ aoc2025/day5/star1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ifstream in("day5.txt");
+    string line;
+    ll l, r, x, ans{0};
+    char c;
+    map<ll, bool> diff;
+    diff[0] = false;
+    while (getline(in, line))
+    {
+        if (line.empty())
+        {
+            break;
+        }
+        istringstream iss(line);
+        iss >> l >> c >> r;
+        map<ll, bool>::iterator it{--diff.lower_bound(l)};
+        bool pre{it->second};
+        ++it;
+        while (it != diff.end() && it->first <= r)
+        {
+            pre = it->second;
+            it = diff.erase(it);
+        }
+        diff[l] = true;
+        diff[r + 1] = it != diff.end() && it->first == r + 1 ? it->second : pre;
+    }
+    while (in >> x)
+    {
+        ans += (--diff.upper_bound(x))->second;
+    }
+    cout << ans << endl;
+    return 0;
+}
