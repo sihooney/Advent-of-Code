d448ea611b175beaef47d8f4ae33b86c7a3fc085
Sihan Qian <84606621+sihooney@users.noreply.github.com>
2025-12-06 01:21:20 -0500

    Implement star1.cpp for day 4 solution

---
+++ aoc2025/day4/star1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("day4.txt");
+    string s;
+    vector<vector<char>> a;
+    int ans{0};
+    int dir[8][2]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    while (in >> s)
+    {
+        a.emplace_back(s.begin(), s.end());
+    }
+    in.close();
+    for (size_t r{0}; r < a.size(); ++r)
+    {
+        for (size_t c{0}, cnt{0}; c < a[r].size(); ++c, cnt = 0)
+        {
+            if (a[r][c] == '.')
+            {
+                continue;
+            }
+            for (const auto &[dr, dc] : dir)
+            {
+                if (r + dr < 0 || r + dr >= a.size() || c + dc < 0 || c + dc >= a[r].size())
+                {
+                    continue;
+                }
+                cnt += a[r + dr][c + dc] == '@';
+            }
+            ans += cnt < 4;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
